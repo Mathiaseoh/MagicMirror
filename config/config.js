39ee18d51d6a532b5b65b403d46bf32b9a6ca4e9
Mathiaseoh <ipadmirror@heidmarks.se>
2025-08-10 18:58:51 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,38 @@
 
+let config = {
+  address: "0.0.0.0",
+  port: 8080,
+  ipWhitelist: [],
+  language: "sv",
+  timeFormat: 24,
+  units: "metric",
+  modules: [
+    {
+      module: "clock",
+      position: "top_left"
+    },
+    {
+      module: "calendar",
+      position: "top_left",
+      config: {
+        calendars: [
+          {
+            url: "https://calendar.google.com/calendar/ical/[DIN_KALENDERLÄNK]/basic.ics",
+            symbol: "calendar"
+          }
+        ]
+      }
+    },
+    {
+      module: "weather",
+      position: "top_right",
+      config: {
+        weatherProvider: "openweathermap",
+        type: "current",
+        location: "Stockholm,SE",
+        apiKey: "[DIN_OPENWEATHER_APIKEY]"
+      }
+    }
+  ]
+};
+module.exports = config;
